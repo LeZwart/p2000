11cb39b2c6a5da06a37091888fe09874e0c6db6b
LeZwart <192443@novacollege.nl>
2025-06-17 10:25:12 +0200

    Full export script

--- export.py
+++ export.py
@@ -2,27 +2,69 @@ import pandas as pd
 import SQLServer
 
 
-def export_to_csv(cursor, filename):
-    """Exports the Bericht table to a CSV file to later be processed in Power BI."""
-
-    query = """
-    SELECT b.id, b.datum, b.beschrijving, b.lat, b.lon, c.capcode
-    FROM Bericht b
-    LEFT JOIN BerichtCapcode bc ON b.id = bc.bericht_id
-    LEFT JOIN Capcode c ON bc.capcode_id = c.id
-    """
-
+def export_query_to_csv(cursor, query, filename):
+    """Helper to export a SQL query to CSV."""
     df = pd.read_sql_query(query, cursor.connection)
-
-    # Handle NaN values for lat and lon
-    df['lat'] = df['lat'].fillna('NaN')
-    df['lon'] = df['lon'].fillna('NaN')
-
     df.to_csv("exports/" + filename, index=False)
-    print(f"Data exported to {filename}")
+    print(f"Exported to {filename}")
 
 
+# Connect to the SQL Server database
 conn = SQLServer.connect("P2000")
 cursor = conn.cursor()
 
-export_to_csv(cursor, 'test.csv')
+# Define and export each query
+queries = {
+    "Bericht.csv": """
+        SELECT
+            id AS bericht_id,
+            datum,
+            beschrijving,
+            lat,
+            lon
+        FROM dbo.Bericht
+    """,
+
+    "BerichtCapcode.csv": """
+        SELECT
+            bericht_id,
+            capcode_id
+        FROM dbo.BerichtCapcode
+    """,
+
+    "Capcode.csv": """
+        SELECT
+            id AS capcode_id,
+            capcode,
+            beschrijving,
+            discipline AS discipline_id,
+            regio AS regio_id,
+            stad AS stad_id
+        FROM dbo.Capcode
+    """,
+
+    "Discipline.csv": """
+        SELECT
+            id AS discipline_id,
+            naam AS discipline_naam
+        FROM dbo.Discipline
+    """,
+
+    "Regio.csv": """
+        SELECT
+            id AS regio_id,
+            naam AS regio_naam
+        FROM dbo.Regio
+    """,
+
+    "Stad.csv": """
+        SELECT
+            id AS stad_id,
+            naam AS stad_naam
+        FROM dbo.Stad
+    """
+}
+
+# Run and export all queries
+for filename, query in queries.items():
+    export_query_to_csv(cursor, query, filename)
